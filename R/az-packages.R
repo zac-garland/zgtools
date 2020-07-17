46dcf5eaa97a4f2ed521c8e0a0d18ba0fc3ce91b
zac-garland <zachary.garland@outlook.com>
2020-07-17 15:42:49 -0600

    added knitr option

--- R/az-packages.R
+++ R/az-packages.R
@@ -1,28 +1,61 @@
 #' @importFrom magrittr %>%
 
+
+
 .onAttach <- function(...) {
   if (!require("pacman")) install.packages("pacman")
-  pacman::p_load(
-    clipr,
-    flexdashboard,
-    here,
-    highcharter,
-    htmlwidgets,
-    janitor,
-    lubridate,
-    kableExtra,
-    pacman,
-    plotly,
-    readxl,
-    rhandsontable,
-    rvest,
-    shiny,
-    shinycssloaders,
-    shinydashboard,
-    shinyWidgets,
-    shinybusy,
-    styler,
-    tidyquant,
-    tidyverse
-  )
+
+  if(isTRUE(getOption('knitr.in.progress'))){
+    highlight_load(
+      clipr,
+      flexdashboard,
+      here,
+      highcharter,
+      htmlwidgets,
+      janitor,
+      lubridate,
+      kableExtra,
+      pacman,
+      plotly,
+      readxl,
+      rhandsontable,
+      rvest,
+      shiny,
+      shinycssloaders,
+      shinydashboard,
+      shinyWidgets,
+      shinybusy,
+      styler,
+      tidyquant,
+      tidyverse
+    )
+
+  } else{
+    pacman::p_load(
+      clipr,
+      flexdashboard,
+      here,
+      highcharter,
+      htmlwidgets,
+      janitor,
+      lubridate,
+      kableExtra,
+      pacman,
+      plotly,
+      readxl,
+      rhandsontable,
+      rvest,
+      shiny,
+      shinycssloaders,
+      shinydashboard,
+      shinyWidgets,
+      shinybusy,
+      styler,
+      tidyquant,
+      tidyverse
+    )
+  }
+
+
+
 }
